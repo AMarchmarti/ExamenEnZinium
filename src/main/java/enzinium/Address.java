cea75a76ece87b4bf6b8705e162e38114bb09e8b
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-20 17:50:30 +0100

    Creacion de claves completada

--- src/main/java/enzinium/Address.java
+++ src/main/java/enzinium/Address.java
@@ -1,4 +1,37 @@
 package enzinium;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 public class Address {
+
+        private PublicKey PK = null;
+        private PrivateKey SK = null;
+
+/*------------------Setters-------------------------------------------*/
+
+        public void setPK(PublicKey PK) {
+            this.PK = PK;
+        }
+
+        public void setSK(PrivateKey SK) {
+            this.SK = SK;
+        }
+
+/*----------------------Getters----------------------------------*/
+
+        public PublicKey getPK() {
+            return PK;
+        }
+
+        public PrivateKey getSK() {
+            return SK;
+        }
+
+/*--------------------Lógica-----------------------------------*/
+
+        public void generateKeyPair(){
+            setPK(GenSig.generateKeyPair().getPublic());
+            setSK(GenSig.generateKeyPair().getPrivate());
+        }
 }
