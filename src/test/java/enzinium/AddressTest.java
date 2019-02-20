b832c157eb80d573cae7e40948d1aac3b3518d25
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-20 17:31:36 +0100

    Añadidas classe gensig y Address

---
+++ src/test/java/enzinium/AddressTest.java
@@ -0,0 +1,17 @@
+package enzinium;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AddressTest
+{
+    @Test
+    public void addressTestClass(){
+        Address address = new Adress();
+        assertNotNull(address);
+
+    }
+}
