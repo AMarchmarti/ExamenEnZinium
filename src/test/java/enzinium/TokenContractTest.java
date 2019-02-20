088402d3314a2e90c206e8a29d2c8fbff0149e04
AMarchmarti <marchmarti.antoni@gmail.com>
2019-02-20 18:25:47 +0100

    Casos test funcionan correctamente

--- src/test/java/enzinium/TokenContractTest.java
+++ src/test/java/enzinium/TokenContractTest.java
@@ -1,4 +1,27 @@
 package enzinium;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TokenContractTest {
+
+    @Test
+    public void contractTest(){
+        Address address = new Address();
+        TokenContract contract = new TokenContract(address);
+        assertNotNull(contract);
+    }
+
+    @Test
+    public void contractSGTest(){
+        Address address = new Address();
+        TokenContract contract = new TokenContract(address);
+        contract.setName("hola");
+        assertEquals("hola", contract.getName());
+        contract.setSymbol("Hola");
+        assertEquals("Hola", contract.getSymbol());
+        contract.setTotalSupply(100);
+        assertEquals(100, contract.getTotalSupply(),0);
+    }
 }
